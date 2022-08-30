cc8ce8467ca51ffe4539ac8ade943afc137cdd7d
javonsher05 <110827599+javonsher05@users.noreply.github.com>
2022-08-30 21:39:21 +0500

    Add files via upload

---
+++ js for/maim.js
@@ -0,0 +1,91 @@
+// 1-misol
+
+// let a=parseInt(prompt("son"))
+// for (let n=0; n<20; n++){
+//     console.log(a);
+// }
+
+// 2-misol
+
+// let a = 30
+// let b = 50
+// for (let i = a; i <= b; i++) {
+//     console.log(i);
+// }
+
+// 3-misol
+
+// let a = 30
+// let b = 50
+// for (let i = a; son <= b; i--) {
+//     console.log(i);
+// }
+
+// 4=misol
+
+// let a = 1500
+// let b = (a / 100) % 10
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// 5-misol
+
+// let a = 1500
+// let b = (a / 100) * 10
+// for (let i = 10; i <= 20; i = i + 2) {
+//     console.log(i * b);
+// }
+
+// 6-misol
+
+// let a = 30
+// let b = 50
+// let c = 0
+// for (let  i= a; i <=b ; i++) {
+//     c=i
+//     console.log(c*a*b);
+// }
+
+// 7-misol
+
+// let a = 30
+// let b = 50
+// let c = 0
+// for (let  i= a; i <=b ; i++) {
+//     c=i
+//     console.log((c**2)*(a**2)*(b**2));
+// }
+
+// 10-misol
+
+// let a = 1
+// let b = 0
+// for (let n = 2; n < 10; n++) {
+//     console.log(n);
+//     b = b - (a / b)
+//     console.log(b);
+// }
+// console.log(b);
+
+// 11-misol
+
+// let a = 0
+// let b = 1
+// for (let n = 0; n < 10; n++) {
+//     s = (a * n)**2
+//     console.log(n);
+// }
+
+// 12-13-misol
+
+// let a = 1.1
+// let b = 1.2
+// let c = 1.3
+// for (let n = 1.1; n > 0; n++) {
+//     s = ((a - n) + c)
+//     console.log(n);
+// }
+
+
+
